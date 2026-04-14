656a04d53983dbcd7b47b8463efb77027adea53b
qqemailisla <wangyiisla2@163.com>
2026-04-14 10:55:01 +0100

    Point app to new Supabase project

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
 ﻿window.CLOUD_CONFIG = {
-  supabaseUrl: "https://ndoiruhjhclugdgnkkkl.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kb2lydWhqaGNsdWdkZ25ra2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNjExMjYsImV4cCI6MjA5MTYzNzEyNn0.sO0Te7pGkKGM4Tg3e1qdvq9OeTP48j33MhdgYpuEMqM",
+  supabaseUrl: "https://wmobppqgtqivfduvidrm.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indtb2JwcHFndHFpdmZkdXZpZHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTAxMjMsImV4cCI6MjA5MTcyNjEyM30.ILoF7BaBmZhFgHHClRQc6KnJI97DeKrpN_FOdUIo9K8",
   otpLength: 6,
 };
